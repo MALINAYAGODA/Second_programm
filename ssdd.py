b6f2dfd415d7b4fac8a0345c0c2f54fe30527d18
Данила Григорьевич Малинин <86769332+MALINAYAGODA@users.noreply.github.com>
2021-11-18 21:00:20 +0300

    ssdd2
    Вова фиг

--- ssdd.py
+++ ssdd.py
@@ -1,2 +1,9 @@
-print('Hello world')
-print()
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+
+
+if __name__ == '__main__':
+    main()
